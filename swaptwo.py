da6e9340bff5f750be10bb84a30d052bf5c1968c
vishnu <vishnu@gmail.com>
2020-10-11 13:46:38 +0530

    swaping two number

---
+++ swaptwo.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number ="))
+num2=int(input("enter second number ="))
+num3=0
+print("Number before Swapping = ",num1)
+print("Number before Swapping = ",num2)
+num3=num1
+num1=num2
+num2=num3
+print("Number After Swapping = ",num1)
+print("Number After Swapping = ",num2)
