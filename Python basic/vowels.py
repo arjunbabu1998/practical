9e32bfeaca87ec2e1af61c6b12bccb273ce8ceac
arjunbabu1998 <arjunbabu2690@gmail.com>
2020-11-10 13:08:05 +0530

    new

---
+++ Python basic/vowels.py
@@ -0,0 +1,7 @@
+word=input("Enter any word:")
+l2=[]
+
+for i in word:
+    if i=='a':
+        l2=i
+print(l2)
--- vowels.py
+++
@@ -1,3 +0,0 @@
-str=char(input("Enter any word"))
-for i in range(0,len(str)):
-    print(i)
